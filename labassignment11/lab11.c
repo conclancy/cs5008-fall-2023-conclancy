72404357016d8b275e1f84127c79e0d51af3d650
Connor Clancy <clancy.mu@gmail.com>
2023-11-29 20:06:40 -0500

    adding code given during lab session

--- labassignment11/lab11.c
+++ labassignment11/lab11.c
@@ -24,12 +24,26 @@ int main() {
   int i, val[5];
   pthread_t threads[5]; 
   
-    
+  // mutex initialize 
+  for(i=0; i<5; i++) {
+    pthread_mutex_init(&chopstick[i], NULL);
+  }
 
+  // pthread create
+  for(i=0; i<5; i++) {
+    val[i] = i;
+    pthread_create(&threads[i], NULL, (void*)philosopher, &val[i]);
+  }
 
+  // wait until all threads are done 
+  for(i=0; i<5; i++) {
+    pthread_join(threads[i], NULL);
+  }
 
+  // destroy mutex
+  for(i=0; i<5; i++) {
+    pthread_mutex_destroy(&chopstick[i]);
+  }
 
-
-
-    return 0;
+  return 0;
 }
