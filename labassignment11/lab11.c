6a3987e189f2d9db9eb9bfdb6cf4998e8407d89a
Connor Clancy <clancy.mu@gmail.com>
2023-11-29 19:42:46 -0500

    update formatting

--- labassignment11/lab11.c
+++ labassignment11/lab11.c
@@ -4,24 +4,23 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <pthread.h>
+
 pthread_mutex_t chopstick[5];//lock variable
 
 // Thread to be executed
-void *philosopher(void *x)
-{
- //Treat variable x as a pointer to an int and then extract the value into n
-    int* a=(int*)x;
-    int n=*a;//dereferencing contents of a will be stored in n
-    
-       /*-----Insert your code here----*/
+void *philosopher(void *x) {
+  //Treat variable x as a pointer to an int and then extract the value into n
+  int* a=(int*)x;
+  int n=*a;//dereferencing contents of a will be stored in n
+  
+  /*-----Insert your code here----*/
    
     
 }
 
 
 /*------------ Main Program---------*/
-int main()
-{
+int main() {
   int i,val[5];
   pthread_t threads[5]; 
   
